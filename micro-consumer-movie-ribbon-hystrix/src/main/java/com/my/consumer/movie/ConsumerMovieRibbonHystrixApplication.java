3e7eaacb2e7059ca2871525a6a834c48e202871c
liuzf <1074166578@qq.com>
2017-10-30 09:58:33 +0800

    # WARNING: head commit changed in the meantime
    提交

---
+++ micro-consumer-movie-ribbon-hystrix/src/main/java/com/my/consumer/movie/ConsumerMovieRibbonHystrixApplication.java
@@ -0,0 +1,29 @@
+package com.my.consumer.movie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author Administrator
+ *
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableCircuitBreaker
+public class ConsumerMovieRibbonHystrixApplication 
+{
+	@Bean
+	@LoadBalanced   //使用ribbon 十分简单1 加依赖 spring-cloud-starter-ribbon，2在ribbon需要加这个注解
+	public RestTemplate restTemplate() {
+			return new RestTemplate();
+	}
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(ConsumerMovieRibbonHystrixApplication.class, args);
+    }
+}
